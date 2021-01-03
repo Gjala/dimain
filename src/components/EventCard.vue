276933dddb517453386dadfd0b903b4c29f74078
George Jalaghonia <georgejalaghonia@Georges-MacBook-Pro.local>
2021-01-03 22:40:47 +0400

    Done

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,32 +1,20 @@
 <template>
+<router-link class="event-link" :to="{name: 'EventDetails', params: {id: event.id} }">
   <div class="event-card">
    <span> @ {{ event.time }} on {{ event.date }} </span>
    <h4>{{ event.title }}</h4>
 
   </div>
+</router-link>
 </template>
 
 <script>
+
 export default {
   name: "EventCard",
-  // props: {
-  //   msg: String
-  // }
- data(){
-   return {
-     event: { 
-      id: 5928101,
-     category: 'animal welfare',
-     title: 'Cat adoption day',
-     description: 'Find your new feline fiend at this event.',
-     location: 'Meow Town',
-     date: 'January 28, 2022',
-     time: '20:00',
-     petsAllowed: true,
-     organizer: 'Kat Loydlee'
-     }
-   }
- }
+  props: {
+    event: Object
+  }
 }
 </script>
 
@@ -43,4 +31,10 @@ export default {
   transform: scale(1.01);
   box-shadow: -3px 12px 0 rgba(0, 0, 0, 0.2);
 }
+
+.event-link {
+  text-decoration: none;
+  color: #2c3e50;
+}
+
 </style>
